34c6d3455dce43a6e3484bdd9214f0e335d67cf7
JWhy <jwhy@jwhy.de>
2012-11-18 15:25:08 +0100

    Better comments in CarpetGenerator
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- app/CarpetGenerator.php
+++ app/CarpetGenerator.php
@@ -5,8 +5,10 @@
  */
 class CarpetGenerator {
 	
+  //Defining standard parameters
 	private $stages = 5;
 	private $canvas_size = 180;
+	private $canvas_dsize = 180;
 
 	public static function startTimetracking(){
 		$t_exp = explode(" ", microtime());
@@ -24,10 +26,13 @@ class CarpetGenerator {
 	
 	/**
 	 * @param Array $args Should contain the keys 'canvas_size' and 'stages' (default: 5)
+   *        canvas_size: Side length of the canvas (defaults to 180px)
+   *        stages: Amount of refinements
 	 */
 	public function __construct($args) {
 		if(isset($args['stages'])) $this->stages = $args['stages'];
 		$this->canvas_size = $args['canvas_size'];
+		$this->canvas_dsize = $args['canvas_dsize'];
 	}
 
 	/**
@@ -36,7 +41,8 @@ class CarpetGenerator {
 	 * @return int Side length of square of given stage
 	 */
 	private function getSquareSidelength($stage, $canvas_size){
-		//<size> / 3^<stage></stage>
+		//<size> / 3^<stage>
+		//pow(number $base, number $exp ) = $base^$exp
 		return($canvas_size/pow(3, $stage));
 	}
 	
@@ -51,28 +57,32 @@ class CarpetGenerator {
 		return($sq_a + 3*($n-1)*$sq_a);
 	}
 	
+	/**
+	 * @return Array with $ret[$stage][$square]
+	 */
 	private function generateCarpet() {
 				
 		$carpet = array();
 		$cs = $this->canvas_size;
 		for($stage = 1; $stage <= $this->stages; $stage++){
-		  //per stage
+		  //Executed per stage
 		  //sq_a: square sidelength
 		  $sq_a = $this->getSquareSidelength($stage, $cs);
 		  
 		  $sq_per_row = pow(3, $stage-1);
 		  $sq_total = $sq_per_row * $sq_per_row;
+		  
 		  for($x_row = 1; $x_row <= $sq_per_row; $x_row++){
-		  	//per x-row
+		  	//Executed per x-row
 		  	$x_row_coord = $sq_a;
-			for($y_row = 1; $y_row <= $sq_per_row; $y_row++){
-				//per y-row on x-row => x/y coordinate
-				$carpet[$stage][] = array(
-					'x' => $this->getSquarePosition($stage, $cs, $x_row),
-					'y' => $this->getSquarePosition($stage, $cs, $y_row),
-					's' => $this->getSquareSidelength($stage, $cs)
-				);
-			}
+        for($y_row = 1; $y_row <= $sq_per_row; $y_row++){
+          //Executed per y-row on x-row => x/y coordinate
+          $carpet[$stage][] = array(
+          'x' => $this->getSquarePosition($stage, $cs, $y_row),
+          'y' => $this->getSquarePosition($stage, $cs, $x_row),
+          's' => $this->getSquareSidelength($stage, $cs)
+          );
+        }
 		  }
 		}
 		return($carpet);
@@ -82,14 +92,15 @@ class CarpetGenerator {
 		$svg = '';
 		$ca = $carpet;
 		foreach($ca as $stageno=>$ca_stage){
-		  //per stage
+		  //Executed per stage
+		  $svg .= "\n<!-- Stage: $stageno -->";
 		  foreach($ca_stage as $sq_array){
-		  	//per square in stage
+		  	//Executed per square in stage
 		  	//sq: square
 		  	(float) $x = $sq_array['x']; //first x-coordinate of square
 		  	(float) $y = $sq_array['y']; //first y-coordinate of square
 		  	(float) $s = $sq_array['s']; //square size
-		  	$svg .= "\n<rect x=\"$x\" y=\"$y\" width=\"$s\" height=\"$s\" /><!-- stage $stageno -->";
+		  	$svg .= "\n<rect x=\"$x\" y=\"$y\" width=\"$s\" height=\"$s\" />";
 		  }	
 		}
 		return($svg);
